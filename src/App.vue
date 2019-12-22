f69840880b72b067427304696a2da47859412420
Ilya-Grigoryev <Ilya-Grigoryev@users.noreply.github.com>
2019-12-22 22:01:00 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
+<!-- Тестирую git commit, не обращайте внимания)) -->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Container>
+      <ChatWindow v-on:send-message="sendMessage">
+        <ChatMessage v-for="message in messages" :key="message.id" :username="message.author" :datetime="message.datetime"><div class="message">{{message.text}}</div></ChatMessage>
+      </ChatWindow>
+    </Container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChatMessage from './components/ChatMessage.vue'
+import ChatWindow from './components/ChatWindow.vue'
+import Container from './components/Container.vue'
+import { delay } from 'q'
 
 export default {
   name: 'app',
+  data(){
+    return{
+      messages: []
+    }
+  },
+  methods: {
+    getMessage(){
+      this.axios.get('http://188.225.47.187/api/chat/getmessages.php')
+      .then((response) => {
+        this.messages = response.data
+        this.messages.reverse()
+      })
+    },
+    sendMessage(newMess){
+      this.axios.post('http://188.225.47.187/api/chat/sendmessage.php',{
+            author: newMess.username,
+            text: newMess.text
+      })
+      delay(100)
+      ChatWindow.nickname = ''
+      ChatWindow.message = ''
+      this.getMessage()
+    }
+  },
+  mounted(){
+    setInterval(this.getMessage(),1)
+  },
   components: {
-    HelloWorld
+    ChatMessage, ChatWindow, Container
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  background-color: #f9f9fa;
+}
+.message{
+  font-size: 20px;
 }
 </style>
